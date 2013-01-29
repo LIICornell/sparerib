c2c2310238383e987cd4919517ee916d245d9873
Andrew Pendleton <apendleton@sunlightfoundation.com>
2013-01-29 01:01:26 -0500

    Improvements to the node renderer.

--- sparerib_node/server.js
+++ sparerib_node/server.js
@@ -2,18 +2,27 @@ var express = require('express');
 var app = express();
 var Browser = require("zombie");
 var http = require('http'), httpProxy = require('http-proxy');
+var fs = require('fs'), _ = require("underscore");
 
 var globalBrowser = new Browser();
 var proxy = new httpProxy.RoutingProxy();
 
-globalBrowser.visit("http://localhost:8005", function() {
+var settingsFile = fs.readFileSync("./settings.json");
+var settings = JSON.parse(settingsFile);
+
+if (fs.existsSync("./local_settings.json")) {
+    var localSettingsFile = fs.readFileSync("./settings.json");
+    _.extend(settings, JSON.parse(localSettingsFile));
+}
+
+globalBrowser.visit("http://" + settings.proxyHost + ":" + settings.proxyPort, function() {
     start();
 })
 
 app.all(/\/(api|static)\/.*/, function(req, res) {
     proxy.proxyRequest(req, res, {
-        host: 'localhost',
-        port: 8005
+        host: settings.proxyHost,
+        port: settings.proxyPort
     });
 });
 
@@ -21,7 +30,6 @@ app.all('*', function(req, res) {
     var browser = globalBrowser.fork();
     browser.wait(function() {
         browser.window.SIMPLE_JS = true;
-        console.log('start wait');
         browser.window.Backbone.history.navigate(req.path, true);
         browser.wait(function() {
             browser.window.$('script').remove();
@@ -32,5 +40,5 @@ app.all('*', function(req, res) {
 
 var start = function() {
     console.log('ready');
-    app.listen(3000);
+    app.listen(settings.port);
 }
