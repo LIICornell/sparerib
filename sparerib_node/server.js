336c2e6139e983fd7ffa0d6de24f3475e2f8d3c8
Andrew Pendleton <abpend@gmail.com>
2013-01-29 01:17:06 -0500

    Use cluster in the node server.

--- sparerib_node/server.js
+++ sparerib_node/server.js
@@ -1,44 +1,63 @@
-var express = require('express');
-var app = express();
-var Browser = require("zombie");
-var http = require('http'), httpProxy = require('http-proxy');
-var fs = require('fs'), _ = require("underscore");
+var cluster = require('cluster');
 
-var globalBrowser = new Browser();
-var proxy = new httpProxy.RoutingProxy();
+if (cluster.isMaster) {
+    var cpuCount = require('os').cpus().length;
 
-var settingsFile = fs.readFileSync("./settings.json");
-var settings = JSON.parse(settingsFile);
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
 
-if (fs.existsSync("./local_settings.json")) {
-    var localSettingsFile = fs.readFileSync("./settings.json");
-    _.extend(settings, JSON.parse(localSettingsFile));
-}
-
-globalBrowser.visit("http://" + settings.proxyHost + ":" + settings.proxyPort, function() {
-    start();
-})
-
-app.all(/\/(api|static)\/.*/, function(req, res) {
-    proxy.proxyRequest(req, res, {
-        host: settings.proxyHost,
-        port: settings.proxyPort
+    cluster.on('exit', function (worker) {
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
     });
-});
 
-app.all('*', function(req, res) {
-    var browser = globalBrowser.fork();
-    browser.wait(function() {
-        browser.window.SIMPLE_JS = true;
-        browser.window.Backbone.history.navigate(req.path, true);
-        browser.wait(function() {
-            browser.window.$('script').remove();
-            res.send(browser.html());
-        })
+} else {
+
+    var express = require('express');
+    var app = express();
+    var Browser = require("zombie");
+    var http = require('http'), httpProxy = require('http-proxy');
+    var fs = require('fs'), _ = require("underscore");
+
+    var globalBrowser = new Browser();
+    var proxy = new httpProxy.RoutingProxy();
+
+    var settingsFile = fs.readFileSync("./settings.json");
+    var settings = JSON.parse(settingsFile);
+
+    if (fs.existsSync("./local_settings.json")) {
+        var localSettingsFile = fs.readFileSync("./local_settings.json");
+        _.extend(settings, JSON.parse(localSettingsFile));
+        console.log(settings);
+    }
+
+    globalBrowser.visit("http://" + settings.proxyHost + ":" + settings.proxyPort, function() {
+        start();
     })
-});
 
-var start = function() {
-    console.log('ready');
-    app.listen(settings.port);
+    app.all(/\/(api|static)\/.*/, function(req, res) {
+        proxy.proxyRequest(req, res, {
+            host: settings.proxyHost,
+            port: settings.proxyPort
+        });
+    });
+
+    app.all('*', function(req, res) {
+        var browser = globalBrowser.fork();
+        browser.wait(function() {
+            browser.window.SIMPLE_JS = true;
+            browser.window.Backbone.history.navigate(req.path, true);
+            browser.wait(function() {
+                browser.window.$('script').remove();
+                res.send(browser.html());
+            })
+        })
+    });
+
+    var start = function() {
+        console.log('ready');
+        app.listen(settings.port);
+    }
 }
